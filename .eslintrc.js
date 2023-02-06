8daf6b07c8710b4d4b4048fff2a6a08c6112f560
Ludovic Muller <ludovicmuller1@gmail.com>
2023-02-06 18:02:26 +0100

    chore: upgrade deps + fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
+    "plugin:import/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "airbnb-typescript/base",
